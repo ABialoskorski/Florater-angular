5bdb6e88f76a535f7513d98ecb44d81d4590fa49
Bialoskorski <artur.bialoskorski@capgemini.com>
2019-07-25 11:25:35 +0200

    made login form with ngmodels on input, next step - send form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule, routingComponent } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+// Angular Material
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule, MatCheckboxModule } from "@angular/material";
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, routingComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule,
+    FormsModule,
+  ],
+
   providers: [],
   bootstrap: [AppComponent],
 })
